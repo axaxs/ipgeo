dd4f119d51796a91413efe042f0410b84dd1d5b1
Alex Skinner <alex.skinner@team.neustar>
2020-01-10 20:13:10 -0500

    dont close db

--- maxminds.go
+++ maxminds.go
@@ -1,30 +1,38 @@
 package main
 
-import ("sync";"os";"log";"net";"io/ioutil";"github.com/oschwald/geoip2-golang";"compress/gzip")
+import (
+	"compress/gzip"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	"github.com/oschwald/geoip2-golang"
+)
 
 var mmr *geoip2.Reader
 var sm sync.Mutex
 
 func init() {
-        f, err := os.Open("/srv/apps/GeoLite2-City.mmdb.gz")
-        if err != nil {
-                log.Fatalf(err.Error())
-        }
-        defer f.Close()
-        gzr, err := gzip.NewReader(f)
-        if err != nil {
-                log.Fatalf(err.Error())
-        }
-        defer gzr.Close()
-        data, err := ioutil.ReadAll(gzr)
-        if err != nil {
-                log.Fatalf(err.Error())
-        }
+	f, err := os.Open("/srv/apps/GeoLite2-City.mmdb.gz")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer f.Close()
+	gzr, err := gzip.NewReader(f)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer gzr.Close()
+	data, err := ioutil.ReadAll(gzr)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 	mmr, err = geoip2.FromBytes(data)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	defer mmr.Close()
 }
 
 func lookupIP(ip string) (*geoip2.City, error) {
@@ -33,4 +41,3 @@ func lookupIP(ip string) (*geoip2.City, error) {
 	d := net.ParseIP(ip)
 	return mmr.City(d)
 }
-
