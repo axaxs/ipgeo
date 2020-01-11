7e62b8676e891c3178d494611e5fbe55029d4eb0
Alex Skinner <alex.skinner@team.neustar>
2020-01-10 22:27:18 -0500

    fix fwd logic

--- ipgeo-standalone.go
+++ ipgeo-standalone.go
@@ -8,35 +8,44 @@ import (
 	"strings"
 )
 
+const BEHIND_PROXY = true
+const IP_HEADER = "X-Real-IP"
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("localhost:9555", nil)
 }
 
-func extip(r *http.Request) string {
+func ipfwd(r *http.Request) (string, []string) {
 	remIP := strings.Split(r.RemoteAddr, ":")[0]
-	remfwd := r.Header.Get("X-Forwarded-For")
-	var answer string
-	if remIP == remfwd || remfwd == "" {
-		answer = remIP
-	} else {
-		answer = fmt.Sprintf("%s fwded for %s", remIP, remfwd)
+	remfwd := strings.Split(r.Header.Get("X-Forwarded-For"), ", ")
+	if BEHIND_PROXY {
+		remIP = r.Header.Get(IP_HEADER)
 	}
+
+	return remIP, remfwd
+}
+
+func extip(r *http.Request) string {
+	ip, fwds := ipfwd(r)
+	answer := ip
+	if len(fwds) > 0 && fwds[0] != "" {
+		answer = fmt.Sprintf("%s fwded for %s", ip, strings.Join(fwds, ", "))
+	}
+
 	return answer + "\n"
 }
 
 func geo(r *http.Request) string {
-	remIP := strings.Split(r.RemoteAddr, ":")[0]
-	remfwd := r.Header.Get("X-Forwarded-For")
-	var ip string
-	if remIP == remfwd || remfwd == "" {
-		ip = remIP
-	} else {
-		ip = remfwd
+	ip, fwds := ipfwd(r)
+	if len(fwds) > 0 {
+		ip = fwds[len(fwds)-1]
 	}
-	if strings.Contains(ip, ",") {
-		ip = strings.TrimSpace(strings.Split(ip, ",")[len(ip)-1])
+
+	if len(r.URL.Query()["ip"]) != 0 {
+		ip = r.URL.Query()["ip"][0]
 	}
+
 	return get_coords(ip)
 }
 
@@ -45,7 +54,20 @@ func get_coords(ip string) string {
 	if err != nil {
 		return "\n"
 	}
-	return fmt.Sprintf("%.4f %.4f\n", city.Location.Latitude, city.Location.Longitude)
+
+	ns := "N"
+	ew := "E"
+	if city.Location.Latitude < 0 {
+		ns = "S"
+		city.Location.Latitude *= -1
+	}
+
+	if city.Location.Longitude < 0 {
+		ew = "W"
+		city.Location.Longitude *= -1
+	}
+
+	return fmt.Sprintf("%.4f %s, %.4f %s\n", city.Location.Latitude, ns, city.Location.Longitude, ew)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
